591edb0dcceed01755c1f47d7e81221c7aec5cce
abutahathetailor-create <abutahathetailor@gmail.com>
2025-11-12 16:59:28 +0300

    Update sw.js
    404 error in mobile page

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'salah-times-v1.2';
+const CACHE_NAME = 'salah-times-v1.3';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
+  './',
+  './index.html',
+  './manifest.json',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
 
@@ -17,29 +17,46 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        }
-        
-        return fetch(event.request).then(function(response) {
-          if(!response || response.status !== 200 || response.type !== 'basic') {
+  // Handle relative paths for GitHub Pages
+  let requestUrl = event.request.url;
+  let baseUrl = self.location.origin + self.location.pathname.replace(/\/[^\/]*$/, '');
+  
+  // If it's a relative path request, reconstruct the full URL
+  if (requestUrl.startsWith(baseUrl + '/salah-times-pwa/')) {
+    // This is a GitHub Pages deployment
+    const relativePath = requestUrl.replace(baseUrl, '');
+    event.respondWith(
+      caches.match('./' + relativePath.split('/').pop())
+        .then(function(response) {
+          return response || fetch(event.request);
+        })
+    );
+  } else {
+    // Normal caching logic
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          if (response) {
             return response;
           }
           
-          var responseToCache = response.clone();
-          
-          caches.open(CACHE_NAME)
-            .then(function(cache) {
-              cache.put(event.request, responseToCache);
-            });
+          return fetch(event.request).then(function(response) {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
             
-          return response;
-        });
-      })
-  );
+            var responseToCache = response.clone();
+            
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+              
+            return response;
+          });
+        })
+    );
+  }
 });
 
 self.addEventListener('activate', function(event) {
