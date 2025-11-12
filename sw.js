1856a5f5252f91980ca8ba0350effaa3986db5d9
abutahathetailor-create <abutahathetailor@gmail.com>
2025-11-12 18:18:18 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,26 @@
-const CACHE_NAME = 'salah-times-v2.1';
+const CACHE_NAME = 'salah-times-v2.2'; // Updated version
+
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './css/style.css',
+  './css/fajr.css',
+  './css/sunrise.css',
+  './css/dhuhr.css',
+  './css/asr.css',
+  './css/maghrib.css',
+  './css/isha.css',
+  './js/app.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
 
 self.addEventListener('install', function(event) {
-    self.skipWaiting(); // Activate immediately
+    self.skipWaiting(); // Force activation
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(function(cache) {
-                return cache.addAll([
-                    './',
-                    './index.html',
-                    './manifest.json',
-                    './css/style.css',
-                    './css/fajr.css',
-                    './css/sunrise.css',
-                    './css/dhuhr.css',
-                    './css/asr.css',
-                    './css/maghrib.css',
-                    './css/isha.css',
-                    './js/app.js',
-                    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
-                ]);
+                return cache.addAll(urlsToCache);
             })
     );
 });
@@ -35,7 +37,7 @@ self.addEventListener('activate', function(event) {
                 })
             );
         }).then(function() {
-            return self.clients.claim(); // Take control immediately
+            return self.clients.claim();
         })
     );
 });
@@ -44,7 +46,6 @@ self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request)
             .then(function(response) {
-                // Return cached version or fetch new
                 return response || fetch(event.request);
             })
     );
