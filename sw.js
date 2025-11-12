bbf487cabe519e70ca7abd7143a91817c8112feb
abutahathetailor-create <abutahathetailor@gmail.com>
2025-11-12 17:57:00 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,15 @@
-const CACHE_NAME = 'salah-times-v1.4';
+const CACHE_NAME = 'salah-times-v2.0';
 const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
   './css/style.css',
+  './css/fajr.css',
+  './css/sunrise.css',
+  './css/dhuhr.css',
+  './css/asr.css',
+  './css/maghrib.css',
+  './css/isha.css',
   './js/app.js',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
